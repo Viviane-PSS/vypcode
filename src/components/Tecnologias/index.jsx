88b90fd33c0b8d37e3d5c1245ef6dc2785b5441b
Viviane Santos <106488860+Viviane-PSS@users.noreply.github.com>
2025-10-24 15:27:37 -0300

    tecnologias mobile ok

--- src/components/Tecnologias/index.jsx
+++ src/components/Tecnologias/index.jsx
@@ -1,29 +1,34 @@
 import { FaJava, FaJs, FaReact, FaCss3Alt, FaHtml5 } from "react-icons/fa";
+import { useIsMobile } from "../../hooks/useIsMobile";
 
 import "./tecnologias.css";
 
 const Tecnologias = () => {
+  const isMobile = useIsMobile();
   return (
     <div id="tecnologias">
       <h2 className="custom-underline">Algumas Tecnologias</h2>
-      <div>
-        <div className="t1">
-          <FaHtml5 />
+      <div className={isMobile ? "col-2" : "col-4"}>
+        <div className="linha">
+          <div className="t1">
+            <FaHtml5 />
+          </div>
+          <div className="t1">
+            <FaCss3Alt />
+          </div>
         </div>
-        <div className="t1">
-          <FaCss3Alt />
-        </div>
+        <div className="linha">
+          <div className="t1">
+            <FaJs />
+          </div>
 
-        <div className="t1">
-          <FaJs />
-        </div>
-
-        <div className="t1">
-          <FaReact />
+          <div className="t1">
+            <FaReact />
+          </div>
         </div>
       </div>
 
-      <div>
+      <div className="linha">
         <div className="t1">
           <FaJava />
         </div>
